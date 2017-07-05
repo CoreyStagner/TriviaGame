f492586dffcc2ffa38e06f51d9387e90fe43cfb4
Corey R. Stagner <cresc0428@gmail.com>
2017-07-05 19:05:55 -0400

    HTML Validation fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	hideMe("#game");
 	hideMe("#results");
+	hideMe(".answerReveal");
 	//Standard Calls
 
 	function log(e){
@@ -11,7 +12,7 @@ $(document).ready(function(){
 
 	//Game Creation
 
-	var counter = 60;
+	var counter = 6;
 	var correctGuess = 0;
 	var incorrectGuess = 0;
 	function hideMe(e){
@@ -28,15 +29,15 @@ $(document).ready(function(){
 		counter--; 
 		$("#announcement").html("<h2 style='text-align:right'>You have " + counter + " seconds remaining!</h2>")
 		if(counter === 0 ){
-			stopGame();
-			$("#announcement").html("<h2 style='text-align:center'>You have run out of time</h2>")
-			hideMe("#game");
-			var pa = confirm("Would you like to try again?")
-				if(pa === true){
-					startGame();
-				} else{
-					showAnswers();
-				}
+			endGame();
+			// $("#announcement").html("<h2 style='text-align:center'>You have run out of time</h2>")
+			// hideMe("#game");
+			// var pa = confirm("Would you like to try again?")
+			// 	if(pa === true){
+			// 		startGame();
+			// 	} else{
+			// 		showAnswers();
+			// 	}
 		}
 	}
 
@@ -46,7 +47,7 @@ $(document).ready(function(){
 				case "radio": this.checked = false;
 			}
 		})
-		counter = 60;
+		counter = 6;
 		correctGuess = 0;
 		incorrectGuess = 0;	
 		timer();
@@ -68,57 +69,19 @@ $(document).ready(function(){
 		showMe("#correctAnswers");
 	}
 
-	// function gameCheck(){
-		
-
-
-	// 	// var q1 = "";
-	// 	// var q2 = "";
-	// 	// var q3 = "";
-	// 	// var q4 = "";
-	// 	// var q5 = "";
-	// 	// var q6 = "";
-	// 	// var q7 = "";
-	// 	// var q8 = "";
-	// 	// var q9 = "";
-	// 	// var q10 = "";
-	// }
-
-	/////////////////////////////////////////////////
-	// Events
-	/////////////////////////////////////////////////
-	
-	$("#newGameBtn").click(function(){
-		startGame();
-	})// End New Game Query
-
-	$("#finishGameBtn").click(function(){
+	function endGame(){
 		clearInterval(remaining);
 		hideMe("#announcement");
-		// Verify that user put an answer for each question
-		// if (!$("input[name='q1']").is("checked") || 
-		// 	!$("#q2").is("checked") ||
-		// 	!$("#q3").is("checked") ||
-		// 	!$("#q4").is("checked") ||
-		// 	!$("#q5").is("checked") ||
-		// 	!$("#q6").is("checked") ||
-		// 	!$("#q7").is("checked") ||
-		// 	!$("#q8").is("checked") ||
-		// 	!$("#q9").is("checked") ||
-		// 	!$("#q10").is("checked")
-		// ) {            
-		// alert("Please answer all of the questions!");        
-		// } else {
 		var a1 = "d";
-		var a2 = "d";
-		var a3 = "d";
-		var a4 = "d";
-		var a5 = "d";
-		var a6 = "d";
-		var a7 = "d";
-		var a8 = "d";
-		var a9 = "d";
-		var a10 = "d";
+		var a2 = "b";
+		var a3 = "a";
+		var a4 = "c";
+		var a5 = "a";
+		var a6 = "b";
+		var a7 = "b";
+		var a8 = "c";
+		var a9 = "b";
+		var a10 = "b";
 		var guess1 = $("input[name='q1']:checked").val();
 		var guess2 = $("input[name='q2']:checked").val();
 		var guess3 = $("input[name='q3']:checked").val();
@@ -223,8 +186,146 @@ $(document).ready(function(){
 			incorrectGuess++;
 			log("You answered Question 10 incorrectly.")
 		}
+
+		$("#correct").text(correctGuess);
+
+
 		hideMe("#game");
 		showMe("#results");
+	}
+
+	/////////////////////////////////////////////////
+	// Events
+	/////////////////////////////////////////////////
+	
+	$("#newGameBtn").click(function(){
+		startGame();
+	})// End New Game Query
+
+	$("#finishGameBtn").click(function(){
+		endGame();
+		// clearInterval(remaining);
+		// hideMe("#announcement");
+		// var a1 = "d";
+		// var a2 = "b";
+		// var a3 = "a";
+		// var a4 = "c";
+		// var a5 = "a";
+		// var a6 = "b";
+		// var a7 = "b";
+		// var a8 = "c";
+		// var a9 = "b";
+		// var a10 = "b";
+		// var guess1 = $("input[name='q1']:checked").val();
+		// var guess2 = $("input[name='q2']:checked").val();
+		// var guess3 = $("input[name='q3']:checked").val();
+		// var guess4 = $("input[name='q4']:checked").val();
+		// var guess5 = $("input[name='q5']:checked").val();
+		// var guess6 = $("input[name='q6']:checked").val();
+		// var guess7 = $("input[name='q7']:checked").val();
+		// var guess8 = $("input[name='q8']:checked").val();
+		// var guess9 = $("input[name='q9']:checked").val();
+		// var guess10 = $("input[name='q10']:checked").val();
+		// log(guess1);
+		// log(guess2);
+		// log(guess3);
+		// log(guess4);
+		// log(guess5);
+		// log(guess6);
+		// log(guess7);
+		// log(guess8);
+		// log(guess9);
+		// log(guess10);
+
+		// if(guess1 === a1){
+		// 	correctGuess++;
+		// 	log("You answered Question 1 correctly.")
+		// } else if(guess1 === "a"){
+		// 	incorrectGuess++;
+		// 	alert("YOU ARE A JERK FOR SAYING THAT I SUCK!!!!!!!!!!!!!!");
+		// 	log("JERK!!! Wrong!"); 
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 1 incorrectly.")
+		// }
+
+		// if(guess2 === a2){
+		// 	correctGuess++;
+		// 	log("You answered Question 2 correctly.")
+		// }  else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 2 incorrectly.")
+		// }
+
+		// if(guess3 === a3){
+		// 	correctGuess++;
+		// 	log("You answered Question 3 correctly.")
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 3 incorrectly.")
+		// }
+
+		// if(guess4 === a4){
+		// 	correctGuess++;
+		// 	log("You answered Question 4 correctly.")
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 4 incorrectly.")
+		// }
+
+		// if(guess5 === a5){
+		// 	correctGuess++;
+		// 	log("You answered Question 5 correctly.")
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 5 incorrectly.")
+		// }
+
+		// if(guess6 === a6){
+		// 	correctGuess++;
+		// 	log("You answered Question 6 correctly.")
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 6 incorrectly.")
+		// }
+
+		// if(guess7 === a7){
+		// 	correctGuess++;
+		// 	log("You answered Question 7 correctly.")
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 7 incorrectly.")
+		// }
+
+		// if(guess8 === a8){
+		// 	correctGuess++;
+		// 	log("You answered Question 8 correctly.")
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 8 incorrectly.")
+		// }
+
+		// if(guess9 === a9){
+		// 	correctGuess++;
+		// 	log("You answered Question 9 correctly.")
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 9 incorrectly.")
+		// }
+
+		// if(guess10 === a10){
+		// 	correctGuess++;
+		// 	log("You answered Question 10 correctly.")
+		// } else {
+		// 	incorrectGuess++;
+		// 	log("You answered Question 10 incorrectly.")
+		// }
+
+		// $("#correct").text(correctGuess);
+
+
+		// hideMe("#game");
+		// showMe("#results");
 	})// End Finish Game Query
 
 	$("#resetGameBtn").click(function(){
